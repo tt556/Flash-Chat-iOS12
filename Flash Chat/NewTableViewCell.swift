351c6df1bf78850dfe77e54b73b0315bd8156158
tt556 <so888tk@gmail.com>
2018-11-28 09:41:46 +0900

    Initial Commit

---
+++ Flash Chat/NewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewTableViewCell.swift
+//  Flash Chat
+//
+//  Created by 神崎泰旗 on 2018/11/27.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
